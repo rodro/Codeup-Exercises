7399fdc785cd899fca6f26fea07cb5038f1f61cd
Gonzalo Rodriguez <g@stm.pm>
2017-07-20 11:32:36 -0500

    Fizz buzz done

---
+++ fizzbuzz.php
@@ -0,0 +1,19 @@
+<?php
+
+for( $number=1; $number<=100; $number++ ){
+  if($number % 15 == 0)  {
+     echo "fizbuzz";
+  } elseif ($number % 5 == 0) {
+     echo "buzz";
+  } elseif ($number % 3 == 0) {
+     echo "fiz";
+  }else {
+    echo $number;
+  }
+  echo "\n";
+
+}
+
+
+
+?>
