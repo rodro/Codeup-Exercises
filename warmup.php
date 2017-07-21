1c2ae0ae789c45623fb57aa02a0e69225292157b
Gonzalo Rodriguez <g@stm.pm>
2017-07-21 13:53:06 -0500

    php exercises

---
+++ warmup.php
@@ -0,0 +1,25 @@
+<?php
+
+// create a variable to store the value of 0
+// foreach through each element in the array
+	// Inside loop, if the element is_numeric...
+		// have another if block to check to convert negative numbers to positive
+		// have another if/else that if the result is odd add it to the result
+		// else, multiply the number to the result
+// end of loop
+// echo the result
+
+$numbers = [2,1,-5,"bob"];
+$result = 0
+foreach ($numbers as $element) {
+	if (is_numeric($element) {
+		if {
+			(element<0)
+		}
+	}
+		$newNumber = $value * 2;
+    echo "$value * 2 is {$newNumber}\n";
+		echo "result";
+}
+
+?>
