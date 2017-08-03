d6c8f5604c06ca3582aab1a7b2f5f44611e97172
Gonzalo Rodriguez <g@stm.pm>
2017-08-03 16:04:35 -0500

    objects commit

--- contact-parser.php
+++ contact-parser.php
@@ -1,29 +1,24 @@
 <?php
-
 function parseContacts($filename)
 {
-    // $contacts = array();
-  $handle = fopen($filename, 'r');
-
-  $contents = fread($handle, filesize($filename));
-  $people = explode ("\n",$contents);
-  $output = [];
-  foreach ($people as $key => $value) {
-      // echo $value . PHP_EOL;
-      $substring = explode("|",$value);
-      $phoneNumber = substr($people[1],0,3);
-
-      // $output[$key] = $people
-      // [
-      //   'name' => 'value',
-      //   'key' => 'value'
-      // ];
-  //     echo phoneNumber[1] . PHP_EOL;
-  // };
-
-
-  fclose($handle);
-  return $output;
+// $contacts = array();
+$handle = fopen($filename, 'r');
+$contents = fread($handle, filesize($filename));
+$people = explode ("\n",$contents);
+$output = [];
+foreach ($people as $key => $value) {
+// echo $value . PHP_EOL;
+$substring = explode("|",$value);
+$phoneNumber = substr($people[1],0,3);
+// $output[$key] = $people
+// [
+//   'name' => 'value',
+//   'key' => 'value'
+// ];
+//     echo phoneNumber[1] . PHP_EOL;
+// };
+fclose($handle);
+return $output;
 }
 $filename = 'contacts.txt';
 // $contents = explode(', ', $filename);
